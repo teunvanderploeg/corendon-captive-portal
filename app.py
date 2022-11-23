e9eab2432ba3bfbfb8b865e381cf5ef47453153f
teunvanderploeg <29253@ma-web.nl>
2022-11-23 12:11:09 +0100

    Clean up database connection

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'user'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'user'
 app.config['MYSQL_DB'] = 'captive_portal_corendon'
-
-app.config["MYSQL_CURSORCLASS"] = "DictCursor"
-app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca": "/path/to/ca-file"}}  # https://mysqlclient.readthedocs.io/user_guide.html#functions-and-attributes
 
 mysql = MySQL(app)
 

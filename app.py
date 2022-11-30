437db832dd549814564f5f42f4e60ca36c03c4e5
teunvanderploeg <29253@ma-web.nl>
2022-11-30 10:15:05 +0100

    Add extra routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -10,13 +10,24 @@ app.config['MYSQL_DB'] = 'captive_portal_corendon'
 
 mysql = MySQL(app)
 
+
 @app.route("/")
-@app.route("/index")
-def index():
-    # cur = mysql.connection.cursor()
-    # cur.execute("SELECT * FROM users")
-    # rv = cur.fetchall()
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+
+    if request.method == 'POST':
+        return render_template("login.html")
 
 
 if __name__ == '__main__':
